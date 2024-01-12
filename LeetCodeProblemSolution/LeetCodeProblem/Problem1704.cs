6493c9c8a453a43d185403dda3e36c94756df63e
Mtonoy <tonoy300@gmail.com>
2024-01-12 20:21:10 +0600

    problem 1704

---
+++ LeetCodeProblemSolution/LeetCodeProblem/Problem1704.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblemSolution.LeetCodeProblem
+{
+    class Problem1704
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            var counter1 = 0;
+            var counter2 = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var lowerCaseCharacter = char.ToLower(s[i]);
+
+                if (lowerCaseCharacter == 'a'
+                    || lowerCaseCharacter == 'e'
+                    || lowerCaseCharacter == 'i'
+                    || lowerCaseCharacter == 'o'
+                    || lowerCaseCharacter == 'u')
+                {
+                    if (i < s.Length / 2)
+                        counter1++;
+                    else
+                        counter2++;
+                }
+            }
+
+            return counter1 == counter2;
+        }
+    }
+}

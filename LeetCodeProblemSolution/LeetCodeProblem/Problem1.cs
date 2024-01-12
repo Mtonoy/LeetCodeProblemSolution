8ca6d1114f31cb297e2d84ba423d7377acfe34ea
Mtonoy <tonoy300@gmail.com>
2024-01-12 19:41:17 +0600

    initials commit

---
+++ LeetCodeProblemSolution/LeetCodeProblem/Problem1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblemSolution.LeetCodeProblem
+{
+    class Problem1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[] { -1, -1 };
+            if (nums == null)
+                return result;
+            if (nums.Length < 2)
+                return result;
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (map.ContainsKey(diff))
+                {
+                    result[0] = map[diff];
+                    result[1] = i;
+                    return result;
+                }
+
+                if (map.ContainsKey(nums[i]))
+                    continue;
+                map.Add(nums[i], i);
+            }
+
+            return result;
+
+
+        }
+    }
+}
---
+++ LeetCodeProblemSolution/Program.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeProblemSolution.LeetCodeProblem;
+using System;
+
+namespace LeetCodeProblemSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem 1
+
+            int[] nums = { 2, 7, 11, 15};
+            int target = 9;
+
+            Problem1 problem1 = new Problem1();
+            int[] output =  problem1.TwoSum(nums, target);
+
+            Console.WriteLine(String.Join(", ", output));
+            Console.ReadLine();
+            #endregion
+
+        }
+    }
+}

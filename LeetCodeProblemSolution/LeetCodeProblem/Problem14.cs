412cdaac3bb709261248a419364de363a9dcc238
Mtonoy <tonoy300@gmail.com>
2024-01-13 21:33:09 +0600

    Problem 14

---
+++ LeetCodeProblemSolution/LeetCodeProblem/Problem14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeProblemSolution.LeetCodeProblem
+{
+    class Problem14
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            int i = 0;
+            StringBuilder str = new StringBuilder ();
+            string shortest = strs.OrderBy(s => s.Length).First();
+
+            foreach (char c in shortest)
+            {
+                if (strs.Any(s => s[i] != c)) break;
+                str.Append(c);
+                i++;
+            }
+
+            return str.ToString();
+        }
+    }
+}

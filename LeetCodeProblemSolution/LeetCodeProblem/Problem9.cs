8eb55d2e186938140d687c19e8a324d6ed0180af
Mtonoy <tonoy300@gmail.com>
2024-01-13 18:45:09 +0600

    Problem 10

---
+++ LeetCodeProblemSolution/LeetCodeProblem/Problem9.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeProblemSolution.LeetCodeProblem
+{
+    class Problem9
+    {
+        public bool IsPalindrome(int x)
+        {
+            return string.Concat(x.ToString().Reverse()) == x.ToString();
+        }
+    }
+}

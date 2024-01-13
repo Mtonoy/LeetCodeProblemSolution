217882c01a54be8350138c561c83d94bdd789775
Mtonoy <tonoy300@gmail.com>
2024-01-13 18:46:09 +0600

    Problem 11

---
+++ LeetCodeProblemSolution/LeetCodeProblem/Problem11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblemSolution.LeetCodeProblem
+{
+    class Problem11
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0, right = height.Length - 1;
+
+            while (left < right)
+            {
+                int h = Math.Min(height[left], height[right]);
+                int width = right - left;
+                maxArea = Math.Max(maxArea, h * width);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}

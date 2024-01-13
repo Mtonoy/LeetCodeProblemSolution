c22828944d5633856f439fc1f911fda54b08e3a6
Mtonoy <tonoy300@gmail.com>
2024-01-13 10:14:03 +0600

    Problem1347

---
+++ LeetCodeProblemSolution/LeetCodeProblem/Problem1347.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblemSolution.LeetCodeProblem
+{
+    class Problem1347
+    {
+        public int MinSteps(string s, string t)
+        {
+            int count = 0;
+            var alphabets = new int[26]; // A-Z alphabets Array
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                //Update frequency of characters in both string
+                alphabets[s[i] - 'a']++;
+                alphabets[t[i] - 'a']--;
+            }
+
+            for (int j = 0; j < alphabets.Length; j++)
+            {
+                if (alphabets[j] > 0)
+                {
+                    count += alphabets[j];
+                }
+            }
+            return count;
+        }
+    }
+}
